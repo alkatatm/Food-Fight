ccf784e0697d8417d90592502ed7416a80ffd8b8
Mustafa Al-Katat <alkatat.m@gmail.com>
2023-10-21 22:33:16 -0700

    Resolved conflict

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const sqlite3 = require('sqlite3').verbose();
 const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken'); // CF: JWT token authentication
 const app = express();
 const port = 3001;
 const db = new sqlite3.Database('database.db');
 const cors = require('cors');
 const multer = require('multer');
+const secretKey = 'secret-key'; // CF: Must be before verifyToken function
+const cookieParser = require('cookie-parser'); // CF: Tokens stored as cookies
+app.use(cookieParser()); // CF: Parsing for tokens as cookies
 const storage = multer.diskStorage({
     destination: function(req, file, cb) {
         cb(null, './uploads'); // You need to create this directory to store uploaded images
@@ -15,6 +19,24 @@ const storage = multer.diskStorage({
     }
 });
 
+//CF: JWT token verification. verifyToken added as second argument in routes to be protected
+function verifyToken(req, res, next) {
+    const token = req.headers.authorization; // CF: assumes token is in header
+    if(!token) {
+        return res.status(401).json({ error: 'Unauthorized'});
+    }
+
+    // CF: Token mismatch
+    jwt.verify(token, secretKey, (err, decoded) => {
+        if(err) {
+            return res.status(403).json({ error: 'Forbidden'});
+        }
+
+        // CF: When token is invalid user data stored in request object
+        req.user = decoded;
+        next();
+    });
+}
 const upload = multer({ storage: storage });
 
 app.use('/uploads', express.static('uploads')); 
@@ -65,37 +87,23 @@ app.post('/login', (req, res) => {
             return res.status(400).json({ success: false, message: 'Invalid credentials' });
         }
 
-        // Authentication successful (not recommended)
-        res.json({ success: true });
+        // CF: Generate JWT token that expires in 1h upon successful login.
+        const token = jwt.sign({username}, secretKey,{expiresIn:'1h'});
+
+        // CF: Set token as HTTP-only cookie for development
+        res.cookie('token', token, {
+            httpOnly: true,
+            secure: false, // CF: for https environments set to true
+            maxAge: 3600000, // CF: 1 hour expiration of token
+        })
+
+        // Authentication successful (not recommended) CF: Token being sent to client on successful login
+        res.json({ success: true, token });
     });
 });
 
-
-app.get('/getid', (req, res) => {
-    const { username } = req.query; // Retrieve username from query string
-    console.log("Received GET request on /getid with username:", username);
-    
-    // Check if a username was provided
-    if (!username) {
-        return res.status(400).json({ error: 'Username is required in the query string' });
-    }
-
-    // Retrieve user from the database based on username
-    db.get('SELECT id FROM users WHERE username = ?', [username], (err, user) => {
-        if (err) {
-            return res.status(500).json({ error: 'Database error' });
-        }
-        if (!user) {
-            return res.status(400).json({ success: false, message: 'User not found' });
-        }
-
-        // Send the user ID as a JSON response
-        res.json({ success: true, userId: user.id });
-    });
-});
-
-
-app.get('/dashboard', (req, res) => {
+// CF: Token verification added, see: 'verifyToken' argument
+app.get('/dashboard', verifyToken, (req, res) => {
     console.log("Received GET request on /dashboard");
     
     db.all('SELECT photos.*, COALESCE(AVG(ratings.rating), 0) AS average_rating FROM photos LEFT JOIN ratings ON photos.id = ratings.photo_id GROUP BY photos.id ORDER BY average_rating DESC;', (err, rows) => {
@@ -116,7 +124,8 @@ app.get('/dashboard', (req, res) => {
     });
 });
 
-app.post('/dashboard/upload', upload.single('image'), (req, res) => {
+// CF: Token verification added, see: 'verifyToken' argument
+app.post('/dashboard/upload', verifyToken, upload.single('image'), (req, res) => {
     const description = req.body.description;
     const imagePath = req.file.path; // This will store the path of the uploaded image
 
@@ -129,9 +138,10 @@ app.post('/dashboard/upload', upload.single('image'), (req, res) => {
     });
 });
 
-app.post('/dashboard/rate', (req, res) => {
-    const user_id = req.body.user_id; // Assuming you send the user's ID
-    const photo_id = req.body.photo_id; // Get the photoId from the request
+// CF: Token verification added, see: 'verifyToken' argument
+app.post('/dashboard/rate', verifyToken, (req, res) => {
+    const user_id = req.body.user_id;
+    const photo_id = req.body.photo_id;
     const rating = req.body.rating;
     
     console.log("Fetching ratings");
