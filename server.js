6fa7a7096b8cdc90eeb53f0693f026b9e74e0aa6
Carlos Fuentes <cffuentes84@gmail.com>
2023-10-21 22:11:34 -0700

    authentication take 2
    made necessary changes to get JWT authentication working and storing tokens as cookies. database changes are just me testing this by adding
    username: user
    password: password

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const sqlite3 = require('sqlite3').verbose();
 const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken'); // CF: JWT token authentication
 const app = express();
 const port = 3001;
 const db = new sqlite3.Database('database.db');
 const cors = require('cors');
 const multer = require('multer');
+const secretKey = 'secret-key'; // CF: Must be before verifyToken function
+const cookieParser = require('cookie-parser'); // CF: Tokens stored as cookies
+app.use(cookieParser()); // CF: Parsing for tokens as cookies
 const storage = multer.diskStorage({
     destination: function(req, file, cb) {
         cb(null, './uploads'); // You need to create this directory to store uploaded images
@@ -15,6 +19,24 @@ const storage = multer.diskStorage({
     }
 });
 
+//CF: JWT token verification. verifyToken added as second argument in routes to be protected
+function verifyToken(req, res, next) {
+    const token = req.headers.authorization; // CF: assumes token is in header
+    if(!token) {
+        return res.status(401).json({ error: 'Unauthorized'});
+    }
+
+    // CF: Token mismatch
+    jwt.verify(token, secretKey, (err, decoded) => {
+        if(err) {
+            return res.status(403).json({ error: 'Forbidden'});
+        }
+
+        // CF: When token is invalid user data stored in request object
+        req.user = decoded;
+        next();
+    });
+}
 const upload = multer({ storage: storage });
 
 app.use('/uploads', express.static('uploads')); 
@@ -65,12 +87,23 @@ app.post('/login', (req, res) => {
             return res.status(400).json({ success: false, message: 'Invalid credentials' });
         }
 
-        // Authentication successful (not recommended)
-        res.json({ success: true });
+        // CF: Generate JWT token that expires in 1h upon successful login.
+        const token = jwt.sign({username}, secretKey,{expiresIn:'1h'});
+
+        // CF: Set token as HTTP-only cookie for development
+        res.cookie('token', token, {
+            httpOnly: true,
+            secure: false, // CF: for https environments set to true
+            maxAge: 3600000, // CF: 1 hour expiration of token
+        })
+
+        // Authentication successful (not recommended) CF: Token being sent to client on successful login
+        res.json({ success: true, token });
     });
 });
 
-app.get('/dashboard', (req, res) => {
+// CF: Token verification added, see: 'verifyToken' argument
+app.get('/dashboard', verifyToken, (req, res) => {
     console.log("Received GET request on /dashboard");
     
     db.all('SELECT photos.*, COALESCE(AVG(ratings.rating), 0) AS average_rating FROM photos LEFT JOIN ratings ON photos.id = ratings.photo_id GROUP BY photos.id ORDER BY average_rating DESC;', (err, rows) => {
@@ -91,7 +124,8 @@ app.get('/dashboard', (req, res) => {
     });
 });
 
-app.post('/dashboard/upload', upload.single('image'), (req, res) => {
+// CF: Token verification added, see: 'verifyToken' argument
+app.post('/dashboard/upload', verifyToken, upload.single('image'), (req, res) => {
     const userId = req.body.user_id;
     const description = req.body.description;
     const imagePath = req.file.path; // This will store the path of the uploaded image
@@ -105,7 +139,8 @@ app.post('/dashboard/upload', upload.single('image'), (req, res) => {
     });
 });
 
-app.post('/dashboard/rate', (req, res) => {
+// CF: Token verification added, see: 'verifyToken' argument
+app.post('/dashboard/rate', verifyToken, (req, res) => {
     const userId = req.body.user_id;
     const photoId = req.body.photo_id;
     const rating = req.body.rating;
